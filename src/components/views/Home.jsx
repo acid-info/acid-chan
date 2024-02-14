f826faea316ae3d4bb46a27ce5a3ee316d228a85
xray <101130721+xray-acid@users.noreply.github.com>
2024-02-14 11:21:35 +0100

    Update Home.jsx

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -196,12 +196,7 @@ const Home = () => {
                   AcidChan is a serverless, adminless, decentralized alternative culture where anyone can free their minds. 
                   We're self organized into a self-organization, We're a video game, We're a masterplan, We're an investment fund, We're a rave, We're saving the internet, 
                   We're God's little warriors, We're Lindy, We're network commandos, we're clear pilled, we're lawyered up in the court of clout.
-                  The working technology comes in part from the 
-                  <a href='https://logos.co/technology' target='_blank' rel='noreferrer'>
-                    Logos Technology Stack
-                  </a>{' '}
-                  protocols using Waku and Codex. it's all text including links from which media is embedded, shared peer-to-peer. Users do not need to register an account before participating
-                  in the community. Feel free to click on a tab below that interests you and jump right in!{' '}
+                  The working technology comes in part from the <a href='https://logos.co/technology' target='_blank' rel='noreferrer'> Logos Technology Stack </a> {' '} protocols using Waku and Codex. it's all text including links from which media is embedded, shared peer-to-peer. Users do not need to register an account before participating in the community. Feel free to click on a tab below that interests you and jump right in!{' '}
                 </p>
                 <br />
                 <p>There are no global rules, each tab is completely independent and its owner decides how/if it should be moderated.</p>
